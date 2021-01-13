c5785026f05262f53ea4fcea17a3d7cfc1a708d9
Cadenze <cadenze@users.noreply.github.com>
2021-01-13 09:46:09 -0800

    Enum types
    Created enum types for corners and edges, preparing for Cube.java overhaul

---
+++ Corner.java
@@ -0,0 +1,7 @@
+/**
+ * Lists the possible colours of the corners of a cube.
+ */
+public enum Corner {
+    WHITE_BLUE_RED, WHITE_ORANGE_BLUE, WHITE_GREEN_ORANGE, WHITE_RED_GREEN,
+    YELLOW_RED_BLUE, YELLOW_BLUE_ORANGE, YELLOW_ORANGE_GREEN, YELLOW_GREEN_RED
+}
---
+++ Edge.java
@@ -0,0 +1,8 @@
+/**
+ * Lists the possible colours of the edges of a cube.
+ */
+public enum Edge {
+    WHITE_RED, WHITE_BLUE, WHITE_ORANGE, WHITE_GREEN,
+    RED_BLUE, ORANGE_BLUE, RED_GREEN, ORANGE_GREEN,
+    YELLOW_RED, YELLOW_BLUE, YELLOW_ORANGE, YELLOW_GREEN
+}
