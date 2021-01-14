e96d229d4038a8b073cb5115d31bd38d99fe3970
Cadenze <cadenze@users.noreply.github.com>
2021-01-13 18:04:21 -0800

    Enum added
    Added colours and methods to retrieve them from the enums

--- Corner.java
+++ Corner.java
@@ -2,6 +2,52 @@
  * Lists the possible colours of the corners of a cube.
  */
 public enum Corner {
-    WHITE_BLUE_RED, WHITE_ORANGE_BLUE, WHITE_GREEN_ORANGE, WHITE_RED_GREEN,
-    YELLOW_RED_BLUE, YELLOW_BLUE_ORANGE, YELLOW_ORANGE_GREEN, YELLOW_GREEN_RED
+    WHITE_BLUE_RED       ("w", "b", "r"),
+    WHITE_ORANGE_BLUE    ("w", "o", "b"),
+    WHITE_GREEN_ORANGE   ("w", "g", "o"),
+    WHITE_RED_GREEN      ("w", "r", "g"),
+    YELLOW_RED_BLUE      ("y", "r", "b"),
+    YELLOW_BLUE_ORANGE   ("y", "b", "o"),
+    YELLOW_ORANGE_GREEN  ("y", "o", "g"),
+    YELLOW_GREEN_RED     ("y", "g", "r");
+
+    private final String colour;
+    private final String colour1;
+    private final String colour2;
+    private static final String ERROR = "corner colour error.";
+
+    Corner(String c, String c1, String c2) {
+        colour = c;
+        colour1 = c1;
+        colour2 = c2;
+    }
+
+    public String colour(int number) {
+        if(number == 0)      { return colour; }
+        else if(number == 1) { return colour1; }
+        else if(number == 2) { return colour2; }
+        else                 { return ERROR; }
+    }
+
+    public String colour(int spin, int number) {
+        switch(spin) {
+            case 0:
+                return colour(number);
+            
+            case 1:
+                if(number == 0)      { return colour2; }
+                else if(number == 1) { return colour; }
+                else if(number == 2) { return colour1; }
+                else                 { return ERROR; }
+
+            case 2:
+                if(number == 0)      { return colour1; }
+                else if(number == 1) { return colour2; }
+                else if(number == 2) { return colour; }
+                else                 { return ERROR; }
+
+            default:
+                return ERROR;
+        }
+    }
 }
--- Edge.java
+++ Edge.java
@@ -2,7 +2,41 @@
  * Lists the possible colours of the edges of a cube.
  */
 public enum Edge {
-    WHITE_RED, WHITE_BLUE, WHITE_ORANGE, WHITE_GREEN,
-    RED_BLUE, ORANGE_BLUE, RED_GREEN, ORANGE_GREEN,
-    YELLOW_RED, YELLOW_BLUE, YELLOW_ORANGE, YELLOW_GREEN
+    WHITE_RED     ("w", "r"),
+    WHITE_BLUE    ("w", "b"),
+    WHITE_ORANGE  ("w", "o"),
+    WHITE_GREEN   ("w", "g"),
+    RED_BLUE      ("r", "b"),
+    ORANGE_BLUE   ("o", "b"),
+    RED_GREEN     ("r", "g"),
+    ORANGE_GREEN  ("o", "g"),
+    YELLOW_RED    ("y", "r"),
+    YELLOW_BLUE   ("y", "b"),
+    YELLOW_ORANGE ("y", "o"),
+    YELLOW_GREEN  ("y", "g");
+
+    private final String colour;
+    private final String colour1;
+    private static final String ERROR = "edge colour error.";
+
+    Edge(String c, String d) {
+        colour = c;
+        colour1 = d;
+    }
+
+    public String colour(int number) {
+        return colour(true, number);
+    }
+    
+    public String colour(boolean parity, int number) {
+        if(parity) {
+            if(number == 0) { return colour; }
+            else if (number == 1) { return colour1; }
+            else { return ERROR; }
+        } else {
+            if(number == 0) { return colour1; }
+            else if (number == 1) { return colour; }
+            else { return ERROR; }
+        }
+    }
 }
